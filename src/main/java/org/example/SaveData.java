944b1dc74a5f0038eae3cdfbfebdfde5e24848a3
charlie-fitzgerald <46912450+charlie-fitzgerald@users.noreply.github.com>
2025-08-14 17:31:33 -0700

    Added GSON dependency to project. Created SaveData class for future save feature

---
+++ src/main/java/org/example/SaveData.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SaveData {
+    private final ArrayList<Task> todo;
+    private final ArrayList<Task> doing;
+    private final ArrayList<Task> done;
+
+    // Default constructor (starts empty)
+    public SaveData() {
+        this(new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+    }
+
+    // Constructor that accepts starting tasks
+    public SaveData(List<Task> todo, List<Task> doing, List<Task> done) {
+        this.todo = new ArrayList<>(todo);
+        this.doing = new ArrayList<>(doing);
+        this.done = new ArrayList<>(done);
+    }
+
+    public List<Task> getTodo() {
+        return todo;
+    }
+
+    public List<Task> getDoing() {
+        return doing;
+    }
+
+    public List<Task> getDone() {
+        return done;
+    }
+
+
+}
